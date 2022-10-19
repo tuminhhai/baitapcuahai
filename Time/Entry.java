763454f3b99af74571d1ff6bec799cca13d89689
tuminhhai <113073765+tuminhhai@users.noreply.github.com>
2022-10-19 19:59:21 +0700

    Add files via upload

---
+++ Time/Entry.java
@@ -0,0 +1,17 @@
+package Time;
+class Entry {
+    public static void main(String[] args) {
+        Time t = new Time(12, 1, 1);
+        t.display();
+        t.setTime(23, 59, 59);
+        t.display();
+        t.nextSecond();
+        t.display();
+        t.nextSecond();
+        t.display();
+        t.setTime(7, 0, 0);
+        t.display();
+        t.previousSecond();
+        t.display();
+    }
+}
